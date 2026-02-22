d8099ce75c5fb13b2b7c1df5639f2c2c32c7b2cf
Mahin556 <mahinraza556@gmail.com>
2026-02-22 11:02:14 +0530

    updated the booking app

--- booking_app/main.go
+++ booking_app/main.go
@@ -3,11 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
+	fmt.Printf("confrenceTicket is %T \nremainingTickets is %T\nconferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to our %s booking application\n", conferenceName)
 	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets uint
+
+	userName = "John Doe"
+
+	println(userName)
+	println(&userName)
+	
+	fmt.Scan(&userName)
+	println(userName)
+
+	fmt.Scan(&userTickets)
+	fmt.Printf("user %v booked %v tickets", userName, userTickets)
 }
